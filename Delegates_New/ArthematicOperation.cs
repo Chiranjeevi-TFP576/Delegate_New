8d8b1adb5974f035beb2d76f662ef268b6dde84c
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-18 22:31:41 +0530

    [chiru] Single Cast Delegate

---
+++ Delegates_New/ArthematicOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_New
+{
+    public delegate int DelgateOperation(int x, int y);
+    public class ArthematicOperation
+    {
+        public static int Addition(int a, int b)
+        {
+            return a+b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ Delegates_New/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Delegates");
+
+            DelgateOperation obj = new DelgateOperation(ArthematicOperation.Addition);
+            DelgateOperation obj1 = new DelgateOperation(ArthematicOperation.Multiplication);
+            Console.WriteLine("{0}", obj(2, 4));
+
+            Console.WriteLine(""+obj1.Invoke(2, 4));
+        }
+    }
+}
